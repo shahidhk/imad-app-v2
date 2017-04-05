17d08d87db503abaff9b11a627a793ff44ad5b0e
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2017-04-05 19:57:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('hello', function(req, res){
+app.get('/hello', function(req, res){
     res.send('World!');
 });
 
